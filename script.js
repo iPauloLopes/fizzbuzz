9f2f0d3abbe4013898ed5bc04f5a0854da8891c4
Paulo Lopes <133612870+iPauloLopes@users.noreply.github.com>
2026-05-28 16:42:20 -0300

    Add code for loop until user chosen number

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("testing")
+let input = parseInt(prompt("Enter ur number and this thing will fizzbuzz it"))
+
+for (let number = 1; number <= input; number++) {
+    if (number === 2) {
+        console.log("test")
+    } else console.log(number)
+}
